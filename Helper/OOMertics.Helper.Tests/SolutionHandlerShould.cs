abf32912bacd4a5bb60820f9e1e3adf65c501f5a
Tomasz Zieleśkiewicz <tomasz.zieleskiewicz@gmail.com>
2022-08-14 20:25:49 +0200

    CI fix

--- Helper/OOMertics.Helper.Tests/SolutionHandlerShould.cs
+++ Helper/OOMertics.Helper.Tests/SolutionHandlerShould.cs
@@ -1,23 +1,26 @@
 using FluentAssertions;
+using System.Reflection;
 
 namespace OOMertics.Helper.Tests
 {
     public class SolutionHandlerShould
     {
-        private static string testSolutionDir = @"E:\Poligon\github\OOMetrics\TestData\TestSolution";
+        private static string solutionLocation = @"../../../../../";
+        private static string testSolutionDir = @"TestData\TestSolution";
         private static string testSolutionName = "TestSolution";
         [Theory]
-        [InlineData(@"E:\Poligon\github\OOMetrics\", "OOMetrics")]
-        [InlineData(@"E:\Poligon\github\OOMetrics\TestData\TestSolution", "TestSolution")]
+        [InlineData(@"", "OOMetrics")]
+        [InlineData(@"TestData\TestSolution", "TestSolution")]
         public async void ProperlyLoadSolutions(string path, string solutionName)
         {
-            var solutionHandler = await SolutionHandler.OpenAsync($"{path}", solutionName);
+            var pathA = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            var solutionHandler = await SolutionHandler.OpenAsync($"{solutionLocation}{path}", solutionName);
             solutionHandler.Projects.Count.Should().BeGreaterThan(0);
         }
         [Fact]
         public async void ProperlyLoadProjects()
         {
-            var solutionHandler = await SolutionHandler.OpenAsync($"{testSolutionDir}", testSolutionName);
+            var solutionHandler = await SolutionHandler.OpenAsync($"{solutionLocation}{testSolutionDir}", testSolutionName);
             var projects = solutionHandler.Projects;
             projects.Count.Should().Be(2);
             projects.Where(p => p.AssemblyName == "TestProject").Count().Should().Be(1);
