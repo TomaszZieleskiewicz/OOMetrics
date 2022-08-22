8552983781a00fddf2028a93552420688fe3a702
Tomasz Zieleśkiewicz <tomasz.zieleskiewicz@gmail.com>
2022-08-22 19:04:47 +0200

    Some proper unit tests...

---
+++ Helper/OOMertics.Helper.Tests/DeclarationShould.cs
@@ -0,0 +1,15 @@
+﻿using OOMertics.Helper.Implementations;
+using OOMetrics.Abstractions;
+
+namespace OOMertics.Helper.Tests
+{
+    public class DeclarationShould
+    {
+        [Fact]
+        public void CreateNewInstance()
+        {
+            var declaration = new Declaration("TestDeclaration", DeclarationType.CLASS_TYPE, "TestNamespace", "TestProject");
+            declaration.Name.Should().Be("TestDeclaration");
+        }
+    }
+}
